2c2c7fe73a624fbf22fc0c8a0da6a825b39cc450
artur.miguel.dias@gmail.com <artur.miguel.dias@gmail.com@563f79fa-9954-11de-a3e0-e3c1cc8a8d16>
2009-09-15 22:22:39 +0000

    Moving all the Mooshak functionality into directory 'mooshak' - step 1

--- trunk/mooshak/lflat.sh
+++ trunk/mooshak/lflat.sh
@@ -4,12 +4,12 @@
 # Runs L-FLAT in the Mooshak context
 
 export LOGTALKHOME=/usr/share/logtalk
-export LOGTALKUSER=${$MOOSHAK_HOME}logtalk
+export LOGTALKUSER=${MOOSHAK_HOME}logtalk
 
 umask 0000
 ln -sf ../hooks.lgt ../lflat.lgt ../loader.lgt $MOOSHAK_LFLAT_HOME
 
 cat > input_test.lgt
-/usr/bin/swilgt -q -g "{'$MOOSHAK_LFLAT_HOME/loader'}, contests::$1('$2', input_test)." | tail --lines=+11
+/usr/bin/swilgt -q -g "{'$MOOSHAK_LFLAT_HOME/mloader'}, contests::$1('$2', input_test)." | tail --lines=+11
 rm -f input_test.lgt input_test.pl $2.pl 
 
